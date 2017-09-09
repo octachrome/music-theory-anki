633e6fda62b4a48aaef9b5201dd167c8219c57a6
Chris Brown <chris@thebrown.net>
2017-09-09 09:38:02 +0100

    Write some basic Anki notes

--- build-notes.js
+++ build-notes.js
@@ -2,7 +2,34 @@ const FIFTHS = [
     'Cb', 'Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#', 'E#'
 ];
 
+const FIELD_DEFS = {
+    MajorName: idx => `${FIFTHS[idx]}`,
+    RelMinorName: idx => `${FIFTHS[idx + 3]}`,
+    Signature: idx => `<img src="${FIFTHS[idx]}_sig.png">`
+}
+
+const fields = Object.keys(FIELD_DEFS);
+console.log('Fields required by note type:\n  ' + fields.join('\n  '));
+
+const lines = [];
+
 for (let i = 1; i < FIFTHS.length - 5; i++) {
+    let line = '';
+    for (let field of fields) {
+        if (line) {
+            line += '; ';
+        }
+        line += FIELD_DEFS[field](i);
+    }
+    lines.push(line);
+}
+
+const stream = require('fs').createWriteStream('import-file.txt');
+stream.write('# ' + fields.join('; ') + '\n');
+stream.write(lines.join('\n') + '\n');
+stream.end();
+
+/*
     let notes = [];
     for (let j = 0; j < 7; j++) {
         notes.push(FIFTHS[i + (j * 2 + 1) % 7 - 1]);
@@ -14,4 +41,4 @@ for (let i = 1; i < FIFTHS.length - 5; i++) {
     console.log(`  IV  ${FIFTHS[i - 1]} major`);
     console.log(`  V   ${FIFTHS[i + 1]} major`);
     console.log(`  vi  ${FIFTHS[i + 3]} minor`);
-}
+*/
