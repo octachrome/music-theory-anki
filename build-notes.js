377ba3e9830ab2e5f9b1cfdf46ed8ef2cb59f8ed
Chris Brown <chris@thebrown.net>
2017-10-08 11:12:59 +0100

    Added minor chords

--- build-notes.js
+++ build-notes.js
@@ -1,5 +1,5 @@
 const FIFTHS = [
-    'Cb', 'Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#', 'E#'
+    'Cb', 'Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#', 'E#', 'B#'
 ];
 
 const FIELD_DEFS = {
@@ -7,7 +7,9 @@ const FIELD_DEFS = {
     RelMinorName: idx => `${FIFTHS[idx + 3]}`,
     Signature: idx => `<img src="${FIFTHS[idx]}_sig.png">`,
     Major_V: idx => `${FIFTHS[idx + 1]}`,
-    Major_IV: idx => `${FIFTHS[idx - 1]}`
+    Major_IV: idx => `${FIFTHS[idx - 1]}`,
+    Minor_ii: idx => `${FIFTHS[idx + 2]}`,
+    Minor_iii: idx => `${FIFTHS[idx + 4]}`
 }
 
 const fields = Object.keys(FIELD_DEFS);
@@ -15,7 +17,7 @@ console.log('Fields required by note type:\n  ' + fields.join('\n  '));
 
 const lines = [];
 
-for (let i = 1; i < FIFTHS.length - 5; i++) {
+for (let i = 1; i <= 13; i++) {
     let line = '';
     for (let field of fields) {
         if (line) {
